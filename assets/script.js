816c3480e3ba87275f33e0c5092bdbd96b3272ec
Aimed0677 <aimed_titouah@protonmail.com>
2024-05-08 19:16:35 +0200

    creation des points bullet

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,40 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
-//Ajout des even listener sur les fleches 
-let arrowClickRight = document.querySelector(".arrow_right");
-arrowClickRight.addEventListener("click", function () {
-	console.log("Vous avez cliqué sur le bouton droit")
-});
-let arrowClickLeft = document.querySelector(".arrow_left");
-arrowClickLeft.addEventListener("click", function () {
-	console.log("Vous avez cliqué sur le bouton gauche")
-});
-//Ajout des bullet points 
-for (let i = 0; i < slides.length; i++) {
-	let bullet = document.createElement("div");
-	let bulletConteneur = document.querySelector(".dots");
-	bulletConteneur.appendChild(bullet);
-	bullet.classList.add("dot");
+    {
+        "image": "slide1.jpg",
+        "tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
+    },
+    {
+        "image": "slide2.jpg",
+        "tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+    },
+    {
+        "image": "slide3.jpg",
+        "tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
+    },
+    {
+        "image": "slide4.png",
+        "tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
+    }
+];
+
+const arrowClickRight = document.querySelector(".arrow_right");
+const arrowClickLeft = document.querySelector(".arrow_left");
+const dotsContainer = document.querySelector(".dots");
+let currentIndex = 0;
+
+// Fonction pour créer les points bullet
+function createDots() {
+    slides.forEach((_dot, index) => {
+        const dot = document.createElement("div");
+		dotsContainer.appendChild(dot);
+        dot.classList.add("dot");
+		if (index === 0) {
+            dot.classList.add("dot_selected");
+        }
+    });
 }
-// differencier le bullet de l'image selectioné 
-let bulletSelected = document.querySelector(".dot")
-if ( slides[0] = 1 ) {
-	bulletSelected.classList.add("dot_selected");
-}
+// Appeler la fonction pour créer les points bullet
+createDots();
+
+
+
