ea78ed0308d34c91621bcdffb7fbc89b104f3a65
Aimed0677 <aimed_titouah@protonmail.com>
2024-05-08 23:40:26 +0200

    mise en place du carrousel

--- assets/script.js
+++ assets/script.js
@@ -36,5 +36,37 @@ function createDots() {
 // Appeler la fonction pour créer les points bullet
 createDots();
 
+// Fonction pour changer de slide
+function changeSlide(index) {
+    const bannerImg = document.querySelector(".banner-img");
+    const tagLine = document.querySelector("#banner p");
 
+    currentIndex = index;
+
+    // Mettre à jour l'image et la ligne de balise
+    bannerImg.src = `./assets/images/slideshow/${slides[index].image}`;
+    tagLine.innerHTML = slides[index].tagLine;
+
+    // Mettre à jour les points bullet
+    const dots = document.querySelectorAll(".dot");
+    dots.forEach((dot, i) => {
+        if (i === index) {
+            dot.classList.add("dot_selected");
+        } else {
+            dot.classList.remove("dot_selected");
+        }
+    });
+}
+
+// Ajouter un écouteur d'événements sur la flèche "droite"
+arrowClickRight.addEventListener("click", () => {
+    currentIndex = (currentIndex + 1) % slides.length;
+    changeSlide(currentIndex);
+});
+
+// Ajouter un écouteur d'événements sur la flèche "gauche"
+arrowClickLeft.addEventListener("click", () => {
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    changeSlide(currentIndex);
+});
 
